e466a8c79c9af7a762de3f31acf31cf2919c5bab
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-06 10:34:32 +0200

    missing "" in MainContent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function MainContent () {
     return (
         <main>
-            <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>I love to visit <span style={{ fontWeight: 'bold', color: 'white' }}>New York, Paris, and Tokyo.</span></p>
+            <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+                "I love to visit <span style={{ fontWeight: 'bold', color: 'white' }}>New York, Paris, and Tokyo."</span></p>
         </main>
     );
 }
