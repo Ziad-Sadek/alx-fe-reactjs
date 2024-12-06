59e6bc73da150b03422385c81721e6c091738c83
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-06 10:44:06 +0200

    MainContent Update

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ function MainContent () {
     return (
         <main>
             <p style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-                ["I love to visit >New York, Paris, and Tokyo."]</p>
+                ["I love to visit New York, Paris, and Tokyo."]</p>
         </main>
     );
 }
