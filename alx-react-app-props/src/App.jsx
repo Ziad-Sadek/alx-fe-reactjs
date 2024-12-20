deb8b3632d049243359a1059d2b86224b8f51229
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-20 21:39:42 +0200

    Refactoring task

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <>
+    <div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+    
+    <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import UserInfo from './UserInfo.Jsx';
+
+function ProfilePage () {
+    return <UserInfo />;
+}
+
+export default ProfilePage;
