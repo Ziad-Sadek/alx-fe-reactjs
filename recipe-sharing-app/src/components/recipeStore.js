74bae9248921885082aff9f537d50721f2cbe2ca
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-25 01:12:48 +0200

    adding updateRecipe and deleteRecipe to zustand store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,9 +3,26 @@ import { create } from 'zustand';
 
 const useRecipeStore = create ((set) => ({
     recipes: [],
+
+    //Adding new recipe:
     addRecipe:  (newRecipe) => set((state) => ({
         recipes: [...state.recipes, newRecipe]
     })),
+
+    //Updating an existing recipe:
+    updateRecipe: (updatedRecipe) => set((state) => ({
+        recipes: state.recipes.map((recipe) =>
+         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    )
+    })), 
+
+    //Deleting a recipe:
+    deleteRecipe: (recipeId) => set((state) => ({
+        recipes: state.recipes.filter((recipe) =>
+        recipe.id !== recipeId
+    )
+    })),
+    
     setRecipes: (recipes) => set({ recipes })
 }));
 
