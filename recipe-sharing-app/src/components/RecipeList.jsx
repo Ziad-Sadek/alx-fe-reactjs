46c922b46cc208dacba313be8b5df70a518dd307
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-25 20:30:19 +0200

    adding some changes

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
 import { useRecipeStore } from "./recipeStore";
 
+
+
 const RecipeList = () => {
     const recipes = useRecipeStore((state) => state.recipes);
+    const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
 
-    return (
+    const displayedRecipes = filteredRecipes.length > 0 ? filteredRecipes : recipes;
+
+
+      return (
         <div>
-            {recipes.map((recipe) => (
-                <div key={recipe.id}>
-                    <h3>{recipe.title}</h3>
-                    <p>{recipe.description}</p>
-                </div>
-            ))}
+            {displayedRecipes.length === 0 ? (
+                <p>No recipes found.</p>
+            ) : (
+                displayedRecipes.map((recipe) => (
+                    <div key={recipe.id}>
+                        <h3>{recipe.title}</h3>
+                        <p>{recipe.description}</p>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
