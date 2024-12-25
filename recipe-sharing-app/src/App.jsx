7c5c77885954d096631797271e7ec03964143bd2
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-25 02:36:38 +0200

    adding routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails'; 
 import AddRecipeForm from './components/AddRecipeForm';
 
 
 const App = () => {
   return (
+    <Router>
     <div>
       <h1>Recipe Sharing App</h1>
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
       <AddRecipeForm />
       <RecipeList />
     </div>
+    </Router>
   );
 };
 
