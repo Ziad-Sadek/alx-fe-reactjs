a497dcd239fb34fc19ea9c44de65d9e56d32f128
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-31 02:33:47 +0200

    updating PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -11,8 +11,17 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  // Using the useQuery hook to fetch posts
-  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts);
+  // Using the useQuery hook with additional configurations
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    'posts', 
+    fetchPosts, 
+    {
+      cacheTime: 1000 * 60 * 10,  // Cache data for 10 minutes (in milliseconds)
+      staleTime: 1000 * 60 * 5,   // Data is fresh for 5 minutes (in milliseconds)
+      refetchOnWindowFocus: true, // Refetch when the window is focused
+      keepPreviousData: true,     // Keep previous data while new data is being fetched
+    }
+  );
 
   // Handle loading state
   if (isLoading) {
@@ -21,23 +30,4 @@ function PostsComponent() {
 
   // Handle error state
   if (isError) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  return (
-    <div>
-      <h1>Posts</h1>
-      <button onClick={() => refetch()}>Refetch Posts</button> {/* Button to refetch data */}
-      <ul>
-        {data.map(post => (
-          <li key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default PostsComponent;
+    return <div>Error: {error.message}</div
