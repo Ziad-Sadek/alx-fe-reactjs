a310f5b5d9d5f2f6a54b6c6e21f5df9850938223
Ziad-Sadek <zezo.sadek56@gmail.com>
2024-12-31 02:49:25 +0200

    adding routing to Post in App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,11 +4,14 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
+import Post from './components/Post';
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/post/:postId" element={<Post />} />
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="*" element={<NotFound />} />
