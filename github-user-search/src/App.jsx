1db3e7db74c2684e689049062cfcbce26fb51d65
Ziad-Sadek <zezo.sadek56@gmail.com>
2025-01-06 00:15:51 +0200

    task 2

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,10 @@
 // src/App.jsx
 import React from 'react';
-import './App.css';
-import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
-      <Search />
+    <div className="text-red-500">
+      Hello, Tailwind CSS is working!
     </div>
   );
 }
