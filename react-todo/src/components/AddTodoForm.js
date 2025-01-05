b1b7d0a89db960aa904b83b905b8049a4a8e46cd
Ziad-Sadek <zezo.sadek56@gmail.com>
2025-01-05 21:17:32 +0200

    importing todoList to app.js

---
+++ react-todo/src/components/AddTodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const AddTodoForm = ({ onAddTodo }) => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.trim()) {
+      onAddTodo(newTodo);
+      setNewTodo(""); // Clear the input after adding the todo
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
