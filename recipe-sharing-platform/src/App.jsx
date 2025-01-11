e5132f41abc9d2b2276c94e16183cff00bfe6da0
Ziad-Sadek <zezo.sadek56@gmail.com>
2025-01-11 23:20:35 +0200

    fixing

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 import { Link } from "react-router-dom";
+import data from "../data.json";
+
+const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    setRecipes(data);
+  }, []);
 
 function App() {
   return (
@@ -10,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} /> 
         <Route path="/recipe/:id" element={<RecipeDetail />} /> 
+        <Link to={`/recipe/${recipe.id}`} className="mt-4 inline-block text-blue-500 hover:underline"></Link>
       </Routes>
     </Router>
   );
